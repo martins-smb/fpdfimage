c64380a6e2d7be9555279a630dcf4344657b1464
Carlos Martins <carlosmartins@smbsolution.com.br>
2015-08-01 14:09:04 -0300

    Adding the library files.

---
+++ FPDFImage.php
@@ -0,0 +1,29 @@
+<?php
+	require('VariableStream.php');
+	require('../../itbz/fpdf/src/fpdf/FPDF.php');
+	
+	class FPDFImage extends FPDF {
+		public function __construct($orientation='P', $unit='mm', $format='A4') {
+			$this->FPDF($orientation, $unit, $format);
+			stream_wrapper_register('var', 'VariableStream');
+		}
+	
+		function setImage($data, $x=null, $y=null, $w=0, $h=0, $link='') {
+			$v = 'img'.md5($data);
+			$GLOBALS[$v] = $data;
+			$a = getimagesize('var://'.$v);
+			if(!$a)
+				$this->Error('Invalid image data');
+			$type = substr(strstr($a['mime'],'/'),1);
+			$this->Image('var://'.$v, $x, $y, $w, $h, $type, $link);
+			unset($GLOBALS[$v]);
+		}
+	
+		function GDImage($im, $x=null, $y=null, $w=0, $h=0, $link='') {
+			ob_start();
+			imagepng($im);
+			$data = ob_get_clean();
+			$this->setImage($data, $x, $y, $w, $h, $link);
+		}
+	}
+?>
